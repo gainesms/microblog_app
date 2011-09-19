310288c24098de39cc23a4352210c031698f4972
Matthew Gaines <gainesms@gmail.com>
2011-09-19 00:35:21 -0500

    added site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,18 +2,49 @@ require 'spec_helper'
 
 describe PagesController do
 
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Microblog | Home")
+    end
+    
+    it "should have body content" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
+
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Microblog | Contact")
+    end
   end
+  
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Microblog | About")
+    end
+  end
+
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,12 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  # Render Views globally
+  config.render_views
+end
+
+# Webrat configuration
+Webrat.configure do |config|
+  config.mode = :rails
 end
